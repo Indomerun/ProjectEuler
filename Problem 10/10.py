77bfdf0aa08d15e032b3fc973d180ed1c48d769b
Indomerun <joelm@chalmers.se>
2016-09-01 12:05:06 +0200

    Add a much faster sieve solution to problem 10

--- Problem 10/10.py
+++ Problem 10/10.py
@@ -1,5 +1,5 @@
-"""Obs!! Slow as fuck"""
-
+"""
+#Obs!! Slow as fuck
 def isPrime(number, primeList):
     for i in primeList:
         if i >= number/2+1:
@@ -16,3 +16,18 @@ while number < 2000000:
     number += 2
     print(number)
 print(sum(primeList))
+"""
+
+limit = 2000000
+marked = [0] * limit
+value = 3
+s = 2
+while value < limit:
+    if marked[value] == 0:
+        s += value
+        i = value
+        while i < limit:
+            marked[i] = 1
+            i += value
+    value += 2
+print(s)
