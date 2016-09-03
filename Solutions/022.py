5e5b99e12c55904307375cf89a2657805c06441a
Indomerun <joelm@chalmers.se>
2016-09-03 21:53:29 +0200

    Rename variable within list comprehension in 022.py

--- Solutions/022.py
+++ Solutions/022.py
@@ -1,2 +1,2 @@
-print(sum([(position+1)*nameScore for position, nameScore in enumerate([sum([ord(char)-64 for char in name]) for name in sorted([numberString for numberString in open("../Data/data_022.txt").read().replace('"', "").split(',')])])]))
+print(sum([(position+1)*nameScore for position, nameScore in enumerate([sum([ord(char)-64 for char in name]) for name in sorted([string for string in open("../Data/data_022.txt").read().replace('"', "").split(',')])])]))
 
