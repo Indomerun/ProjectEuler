8ac36e0caae0a347d0796ae099ff9656113055c5
Indomerun <joelm@chalmers.se>
2016-09-05 00:19:24 +0200

    Rename functions in 044.py

--- Solutions/044.py
+++ Solutions/044.py
@@ -3,7 +3,7 @@ import math
 def P(n):
     return n*(3*n-1)//2
 
-def isPenta(num):
+def isPentagonal(num):
     return (1 + math.sqrt(1 + 24*num)) % 6 == 0
 
 limitJ = 10000
@@ -11,7 +11,7 @@ limitI = 2000
 found = False
 for j in range(1,limitJ):
     for i in range(1, limitI):
-        if isPenta(P(j+i) - P(j)) & isPenta(P(j+i) + P(j)):
+        if isPentagonal(P(j+i) - P(j)) & isPentagonal(P(j+i) + P(j)):
             print(P(j+i) - P(j))
             found = True
             break
