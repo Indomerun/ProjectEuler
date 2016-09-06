fc2d3853026978b3a7d9099da27bc5a344b4c94e
Joakim Strandberg <joakim.u.strandberg@gmail.com>
2016-09-06 23:18:06 +0200

    Pythonifiera koden
    Föregående kod är helt klart opythonsk och utnyttjar for loopar på ett väldigt javaistiskt sätt. Den föreslagna koden är mycket mer läsbar, löser problemet med mindre kod, och utnyttjar pythons inbyggda loop-funktioner mer effektivt.

--- Solutions/016.py
+++ Solutions/016.py
@@ -1,15 +1,1 @@
-import math
-
-powOf2 = 1000
-limit = int(math.log10(2)*powOf2)+1
-numberList = [0]*limit
-numberList[0] = 1
-
-for i in range(0,powOf2):
-    for j in range(0,limit):
-        numberList[j] *= 2
-    for j in range(0,limit):
-        if numberList[j] >= 10:
-            numberList[j+1] += (numberList[j]-numberList[j]%10)//10
-            numberList[j] %= 10
-print(sum(numberList))
+sum([int(char) for char in str(2**1000)])
