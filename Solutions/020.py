b3fde78bbb3d46c6e6ce3c4a6d3554019ac5410c
Indomerun <joelm@chalmers.se>
2016-09-13 19:17:23 +0200

    Update 020.py using the fact that Python has no Int-limit

--- Solutions/020.py
+++ Solutions/020.py
@@ -1,13 +1,3 @@
-faculty = 100
-limit = 2*faculty
-numberList = [0]*limit
-numberList[0] = 1
+import math
 
-for i in range(1,faculty+1):
-    for j in range(0,limit):
-        numberList[j] *= i
-    for j in range(0,limit):
-        if numberList[j] >= 10:
-            numberList[j+1] += (numberList[j]-numberList[j]%10)//10
-            numberList[j] %= 10
-print(sum(numberList))
+print(sum([int(i) for i in str(math.factorial(1000))]))
