c11d3131fdc330b51564587f20428204b1264bb0
Indomerun <joelm@chalmers.se>
2016-09-18 17:02:35 +0200

    Add improved and much simplified solution to problem 26

--- Solutions/026.py
+++ Solutions/026.py
@@ -1,43 +1,18 @@
-def nextDecimal(decimal,d):
-    return (10 * decimal[1]) // d, (10 * decimal[1]) % d
-
-limit = 10000
-max_d = 999
-cycles = [0] * max_d
-for d in range(1,max_d+1):
-    decimal1 = (1,1)
-    d1 = d
-    while d1 != 0:
-        d1 //= 10
-        decimal1 = nextDecimal(decimal1, d)
-    decimal2 = nextDecimal(decimal1, d)
-    period = 1
-    for i in range(10000):
-        done = True
-        decimal1 = nextDecimal(decimal1, d)
-        decimal2 = nextDecimal(decimal2, d)
-        while period < limit:
-            if decimal1[0] == decimal2[0]:
-                break
-            decimal2 = nextDecimal(decimal2, d)
-            period += 1
-        if period < limit:
-            for j in range((1+1000//period)*period):
-                decimal1 = nextDecimal(decimal1, d)
-                decimal2 = nextDecimal(decimal2, d)
-                if decimal1[0] != decimal2[0]:
-                    done = False
-                    break
-            if done:
-                break
+limit = 1000
+largestPeriod = 0
+for d in range(1, limit):
+    rest = 1
+    decimals = {}
+    i = 0
+    while True:
+        (decimal, rest) = ((10*rest)//d, (10*rest) % d)
+        if (decimal, rest) in decimals:
+            period = i-decimals[(decimal, rest)]
+            break
         else:
-            period = 1
-    if (period == 1) & (decimal1[0] == 0) & (decimal2[0] == 0):
-        period = 0
-    cycles[d-1] = period
-max_cycle = max(cycles)
-for i, n in enumerate(cycles):
-    if n == max_cycle:
-        break
-print(i+1, max_cycle)
-
+            decimals[(decimal, rest)] = i
+        i += 1
+    if period > largestPeriod:
+        largestPeriod = period
+        answer = d
+print(answer)
